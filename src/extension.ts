358d797e611fa77ff8ed7e61921451c0d6890c7c
caribouHY <oririyz@gmail.com>
2024-08-08 23:31:29 +0900

    add outline

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "si-r-config" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('si-r-config.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Si-R_config!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider({ language: "si-r" }, new OutlineProvider()));
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+class OutlineProvider {
+
+	async provideDocumentSymbols(document: vscode.TextDocument) {
+		console.log('eval');
+		let symbols: vscode.DocumentSymbol[] = [];
+		let last_cmd = "";
+		let last_start = document.lineAt(0).range.start;
+
+		for (let i = 0; i < document.lineCount; i++) {
+			const line = document.lineAt(i);
+			const cmd = line.text.substring(0, line.text.indexOf(' '));
+			if (cmd === '' || cmd[0] === '#') {
+				continue;
+			}
+			if (cmd !== last_cmd) {
+				if (last_cmd !== "") {
+					const range = new vscode.Range(last_start, document.lineAt(i-1).range.end);
+					const symbol = new vscode.DocumentSymbol(last_cmd, '', vscode.SymbolKind.Field, range, range);
+					symbols.push(symbol);
+				}
+				last_cmd = cmd;
+				last_start = line.range.start;
+			}
+		}
+		if (last_cmd !== "") {
+			const range = new vscode.Range(last_start, document.lineAt(document.lineCount-1).range.end);
+			const symbol = new vscode.DocumentSymbol(last_cmd, '', vscode.SymbolKind.Field, range, range);
+			symbols.push(symbol);
+		}
+		return symbols;
+	}
+
+}
